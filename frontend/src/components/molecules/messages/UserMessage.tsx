5d855b66c77a0bcfd6573d8a4139b01abe84bf04
Udaya Raj Panday <54113414+udayarpanday@users.noreply.github.com>
2025-01-10 14:38:03 +0545

    Revert "Fix/dashboard layout"

--- frontend/src/components/molecules/messages/UserMessage.tsx
+++ frontend/src/components/molecules/messages/UserMessage.tsx
@@ -1,7 +1,6 @@
 import { MessageContext } from 'contexts/MessageContext';
-import { useContext, useRef, useState, useEffect } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
-import { WidgetContext } from 'context';
 
 import { Box, IconButton, Stack, TextField } from '@mui/material';
 
@@ -16,7 +15,6 @@ import { AccentButton, RegularButton } from 'components/atoms/buttons';
 import { Translator } from 'components/i18n';
 
 import PencilIcon from 'assets/pencil';
-import { MessageAvatar } from './components/Avatar';
 
 interface Props {
   message: IStep;
@@ -27,13 +25,11 @@ export default function UserMessage({
   children
 }: React.PropsWithChildren<Props>) {
   const config = useConfig();
-  const { evoya } = useContext(WidgetContext);
   const { askUser, loading } = useContext(MessageContext);
   const { editMessage } = useChatInteract();
   const setMessages = useSetRecoilState(messagesState);
   const disabled = loading || !!askUser;
   const [isEditing, setIsEditing] = useState(false);
-  const [layout, setLayout] = useState(false);
   const textFieldRef = useRef<HTMLInputElement>(null);
 
   const isEditable = !!config.config?.features.edit_message;
@@ -55,24 +51,15 @@ export default function UserMessage({
     }
   };
 
-  useEffect(() => {
-    if(evoya?.type === 'dashboard' || config.config?.ui.cot ==='full'){
-      setLayout(true)
-    }
-  }, [evoya,config])
-  
-
   return (
     <Box display="flex" flexDirection="column" width="100%">
       <Box
         display="flex"
-        flexDirection={layout ? "row-reverse":'row'}
-        justifyContent={layout ? "flex-end":'flex-start'}
-        alignItems={layout ? "start":'center'}
+        flexDirection="row"
+        alignItems="center"
         gap={1}
         width="100%"
         sx={{
-          marginBottom:layout?'15px':'0',
           '&:hover .edit-icon': {
             visibility: 'visible'
           }
@@ -81,7 +68,7 @@ export default function UserMessage({
         {!isEditing && isEditable && (
           <IconButton
             sx={{
-              ml: layout ? '0':'auto',
+              ml: 'auto',
               visibility: 'hidden'
             }}
             className="edit-icon"
@@ -91,61 +78,55 @@ export default function UserMessage({
             <PencilIcon sx={{ height: 16, width: 16 }} />
           </IconButton>
         )}
-        {
-          layout &&
-          <Box sx={{order:1,marginTop:'18px'}}>
-            <MessageAvatar author={evoya?.username ?? 'You'} />
-          </Box>
-          }
-          <Box
-            sx={{
-              px: 2.5,
-              position: 'relative',
-              borderRadius: '1.5rem',
-              backgroundColor: 'background.paper',
-              width: isEditing ? '100%' : 'auto',
-              maxWidth: isEditing ? '100%' : '70%',
-              flexGrow: isEditing ? 1 : 0,
-              ml: isEditable ? 'default' : 'auto'
-            }}
-          >
-            {isEditing ? (
-              <Stack py={1.5}>
-                <TextField
-                  id="edit-chat-input"
-                  multiline
-                  autoFocus
-                  variant="standard"
-                  autoComplete="off"
-                  defaultValue={message.output}
-                  fullWidth
-                  inputRef={textFieldRef}
-                  InputProps={{
-                    disableUnderline: true,
-                    sx: {
-                      pl: 0,
-                      width: '100%'
-                    }
-                  }}
-                />
-                <Box display="flex" justifyContent="flex-end" gap={1}>
-                  <RegularButton onClick={() => setIsEditing(false)}>
-                    <Translator path="components.molecules.newChatDialog.cancel" />
-                  </RegularButton>
-                  <AccentButton
-                    className="confirm-edit"
-                    disabled={disabled}
-                    variant="outlined"
-                    onClick={handleEdit}
-                  >
-                    <Translator path="components.molecules.newChatDialog.confirm" />
-                  </AccentButton>
-                </Box>
-              </Stack>
-            ) : (
-              children
-            )}
-          </Box>
+        <Box
+          sx={{
+            px: 2.5,
+            position: 'relative',
+            borderRadius: '1.5rem',
+            backgroundColor: 'background.paper',
+            width: isEditing ? '100%' : 'auto',
+            maxWidth: isEditing ? '100%' : '70%',
+            flexGrow: isEditing ? 1 : 0,
+            ml: isEditable ? 'default' : 'auto'
+          }}
+        >
+          {isEditing ? (
+            <Stack py={1.5}>
+              <TextField
+                id="edit-chat-input"
+                multiline
+                autoFocus
+                variant="standard"
+                autoComplete="off"
+                defaultValue={message.output}
+                fullWidth
+                inputRef={textFieldRef}
+                InputProps={{
+                  disableUnderline: true,
+                  sx: {
+                    pl: 0,
+                    width: '100%'
+                  }
+                }}
+              />
+              <Box display="flex" justifyContent="flex-end" gap={1}>
+                <RegularButton onClick={() => setIsEditing(false)}>
+                  <Translator path="components.molecules.newChatDialog.cancel" />
+                </RegularButton>
+                <AccentButton
+                  className="confirm-edit"
+                  disabled={disabled}
+                  variant="outlined"
+                  onClick={handleEdit}
+                >
+                  <Translator path="components.molecules.newChatDialog.confirm" />
+                </AccentButton>
+              </Box>
+            </Stack>
+          ) : (
+            children
+          )}
+        </Box>
       </Box>
     </Box>
   );
